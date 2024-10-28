bd468c99c94d70de653824abf2efaad0d2673004
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-10-28 18:59:53 -0300

    projeto inciado

---
+++ src/main/java/com/projetoII/cupCake/CupCakeApplication.java
@@ -0,0 +1,13 @@
+package com.projetoII.cupCake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupCakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoII/cupCake/CupCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoII.cupCake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
