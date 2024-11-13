e17d719ce157ae77d67ca8e54478afce4f17f2c3
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-13 14:30:47 -0300

    Adicao da tratativa para mostrar a lista de cupcake na pagina home

--- src/main/java/br/com/projeto/cupCake/controller/HomeController.java
+++ src/main/java/br/com/projeto/cupCake/controller/HomeController.java
@@ -1,17 +1,28 @@
 package br.com.projeto.cupCake.controller;
 
+import br.com.projeto.cupCake.dto.CupCakeDTO;
+import br.com.projeto.cupCake.service.CupCakeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/home")
 public class HomeController {
 
+    private final CupCakeService cupCakeService;
+
     @GetMapping
     public ModelAndView home() {
-        return new ModelAndView("home");
+        ModelAndView mv = new ModelAndView("home");
+        List<CupCakeDTO> cupCakes = cupCakeService.buscarTudo();
+        mv.addObject("cupCakes", cupCakes);
+        return mv;
     }
 
 }
