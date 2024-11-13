39491cf962397f55a2b03fa6a2e2a3fd70ac2053
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-13 12:47:50 -0300

    projeto criado

---
+++ src/main/java/br/com/projeto/cupCake/CupCakeApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.cupCake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupCakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/cupCake/CupCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.cupCake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
