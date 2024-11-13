40184605c2f6cc0a338b3d8f8cae7586a3d852c5
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-13 15:32:45 -0300

    Adicao do ReceitaController

---
+++ src/main/java/br/com/projeto/cupCake/controller/ReceitaController.java
@@ -0,0 +1,31 @@
+package br.com.projeto.cupCake.controller;
+
+import br.com.projeto.cupCake.dto.CupCakeDTO;
+import br.com.projeto.cupCake.model.CupCake;
+import br.com.projeto.cupCake.service.CupCakeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/receita")
+public class ReceitaController {
+
+    private final CupCakeService cupCakeService;
+
+    @GetMapping
+    public ModelAndView receita() {
+        return new ModelAndView("receita");
+    }
+
+    @PostMapping("/novo")
+    public ModelAndView novoReceita(CupCakeDTO dto) {
+        cupCakeService.salvar(dto);
+        return new ModelAndView("redirect:/home");
+    }
+
+}
