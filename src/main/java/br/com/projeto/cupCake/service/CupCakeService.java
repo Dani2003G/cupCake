7b5f574dfbea57dc7792a2aacbde025f5daf7934
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-13 14:29:55 -0300

    Adicao do CupCakeService e do metodo buscarTudo que transforma a entidade em dto

---
+++ src/main/java/br/com/projeto/cupCake/service/CupCakeService.java
@@ -0,0 +1,21 @@
+package br.com.projeto.cupCake.service;
+
+import br.com.projeto.cupCake.dto.CupCakeDTO;
+import br.com.projeto.cupCake.model.CupCake;
+import br.com.projeto.cupCake.repository.CupCakeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CupCakeService {
+
+    private final CupCakeRepository cupCakeRepository;
+
+    public List<CupCakeDTO> buscarTudo() {
+        List<CupCake> cupCakes = cupCakeRepository.findAll();
+        return cupCakes.stream().map(new CupCakeDTO()::toDTO).toList();
+    }
+}
