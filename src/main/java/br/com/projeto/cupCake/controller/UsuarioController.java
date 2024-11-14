68cb441875581fa0017bbf1b5d1fe6a00bb407cc
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-14 12:05:19 -0300

    Adicao da rota para pegar a pagina do cartao e a rota para cadastrar um novo cartao

--- src/main/java/br/com/projeto/cupCake/controller/UsuarioController.java
+++ src/main/java/br/com/projeto/cupCake/controller/UsuarioController.java
@@ -1,10 +1,14 @@
 package br.com.projeto.cupCake.controller;
 
+import br.com.projeto.cupCake.dto.CartaoDTO;
 import br.com.projeto.cupCake.dto.HistoricoPedidoDTO;
+import br.com.projeto.cupCake.service.CartaoService;
 import br.com.projeto.cupCake.service.HistoricoPedidoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,6 +21,8 @@ public class UsuarioController {
 
     private final HistoricoPedidoService historicoPedidoService;
 
+    private final CartaoService cartaoService;
+
     @GetMapping
     public ModelAndView perfil() {
         return new ModelAndView("/usuario/perfil");
@@ -28,6 +34,17 @@ public class UsuarioController {
         List<HistoricoPedidoDTO> historicoPedidos = historicoPedidoService.buscarTudo();
         mv.addObject("historicoPedidos", historicoPedidos);
         return mv;
+    }
+
+    @GetMapping("/cartao")
+    public ModelAndView cartao() {
+        return new ModelAndView("/usuario/cartao");
+    }
+
+    @PostMapping("/cartao/novo")
+    public ModelAndView cartao(CartaoDTO dto) {
+        cartaoService.salvar(dto);
+        return new ModelAndView("redirect:/usuario");
     }
 
 }
