7e45e13c9eb3cf1bee56ff0ab48251aeee1e194c
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-16 00:48:41 -0300

    Adicao da tabela Usuario com joins na tabela HistoricoPedido e CupCake

---
+++ src/main/java/br/com/projeto/cupCake/model/Usuario.java
@@ -0,0 +1,50 @@
+package br.com.projeto.cupCake.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String cpf;
+
+    private String senha;
+
+    private LocalDate dataNascimento;
+
+    private String estado;
+
+    private String cidade;
+
+    private String endereco;
+
+    private String role;
+
+    private LocalDateTime dataCadastro;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuario")
+    private List<HistoricoPedido> historicoPedidos;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuario")
+    private List<CupCake> cupCakes;
+
+}
