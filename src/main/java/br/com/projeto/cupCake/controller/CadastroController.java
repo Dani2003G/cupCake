ec0023436d65fbf5cbdc271a1bb8e70447b866d6
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-16 01:02:14 -0300

    Adicao do metodo post cadastrar

--- src/main/java/br/com/projeto/cupCake/controller/CadastroController.java
+++ src/main/java/br/com/projeto/cupCake/controller/CadastroController.java
@@ -1,15 +1,27 @@
 package br.com.projeto.cupCake.controller;
 
+import br.com.projeto.cupCake.dto.UsuarioDTO;
+import br.com.projeto.cupCake.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
 public class CadastroController {
 
+    private final UsuarioService usuarioService;
+
     @GetMapping("/cadastrar")
-    public ModelAndView cadastro() {
+    public ModelAndView cadastro(UsuarioDTO dto) {
         return new ModelAndView("cadastro");
     }
 
+    @PostMapping("/cadastrar")
+    public ModelAndView cadastrar(UsuarioDTO dto) {
+        usuarioService.salvar(dto);
+        return new ModelAndView("cadastro");
+    }
 }
