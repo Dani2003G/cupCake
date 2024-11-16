09eb55ad0a1d626c0417499142b0e88af583d0b7
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-16 01:01:29 -0300

    Vinculando o receita com o usuario

--- src/main/java/br/com/projeto/cupCake/service/CupCakeService.java
+++ src/main/java/br/com/projeto/cupCake/service/CupCakeService.java
@@ -3,6 +3,7 @@ package br.com.projeto.cupCake.service;
 import br.com.projeto.cupCake.dto.CupCakeDTO;
 import br.com.projeto.cupCake.model.CupCake;
 import br.com.projeto.cupCake.repository.CupCakeRepository;
+import br.com.projeto.cupCake.repository.UsuarioRepositoy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +15,16 @@ public class CupCakeService {
 
     private final CupCakeRepository cupCakeRepository;
 
+    private final UsuarioRepositoy usuarioRepositoy;
+
     public List<CupCakeDTO> buscarTudo() {
         List<CupCake> cupCakes = cupCakeRepository.findAll();
         return cupCakes.stream().map(new CupCakeDTO()::toDTO).toList();
     }
 
-    public void salvar(CupCakeDTO dto) {
-        cupCakeRepository.save(dto.toEntity());
+    public void salvar(CupCakeDTO dto, String email) {
+        CupCake cupCake = dto.toEntity();
+        cupCake.setUsuario(usuarioRepositoy.findByEmail(email));
+        cupCakeRepository.save(cupCake);
     }
 }
