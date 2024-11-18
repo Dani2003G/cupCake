e8ff68887bd80ed580395e0bd54e1143b1b3b1d0
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-18 14:56:52 -0300

    Adicao do atributo preco

--- src/main/java/br/com/projeto/cupCake/model/CupCake.java
+++ src/main/java/br/com/projeto/cupCake/model/CupCake.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity
 @Getter
 @Setter
@@ -25,6 +27,8 @@ public class CupCake {
     private String modoPreparo;
 
     private String urlImagem;
+
+    private BigDecimal preco;
 
     @Enumerated(EnumType.STRING)
     private Tipo tipo;
