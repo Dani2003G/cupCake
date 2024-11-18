56c1ee73d1f4e17311e8db85143ac909b168ce41
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-18 09:38:03 -0300

    Adicao do metodo buscarPorUsuario

--- src/main/java/br/com/projeto/cupCake/repository/CupCakeRepository.java
+++ src/main/java/br/com/projeto/cupCake/repository/CupCakeRepository.java
@@ -2,8 +2,15 @@ package br.com.projeto.cupCake.repository;
 
 import br.com.projeto.cupCake.model.CupCake;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CupCakeRepository extends JpaRepository<CupCake, Long> {
+
+    @Query("SELECT cc FROM CupCake cc JOIN cc.usuario u WHERE  u.email = :email")
+    List<CupCake> buscarPorUsuario(@Param("email") String email);
 }
