fb934e22b3563002128a29a20a31c2177a008d51
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-19 14:01:45 -0300

    Adicao dos metodo buscarPorId e buscarFavoritosPorUsuario

--- src/main/java/br/com/projeto/cupCake/service/CupCakeService.java
+++ src/main/java/br/com/projeto/cupCake/service/CupCakeService.java
@@ -3,12 +3,17 @@ package br.com.projeto.cupCake.service;
 import br.com.projeto.cupCake.dto.CupCakeDTO;
 import br.com.projeto.cupCake.enums.Tipo;
 import br.com.projeto.cupCake.model.CupCake;
+import br.com.projeto.cupCake.model.Favorito;
+import br.com.projeto.cupCake.model.Usuario;
 import br.com.projeto.cupCake.repository.CupCakeRepository;
+import br.com.projeto.cupCake.repository.FavoritoRepository;
 import br.com.projeto.cupCake.repository.UsuarioRepositoy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +22,8 @@ public class CupCakeService {
     private final CupCakeRepository cupCakeRepository;
 
     private final UsuarioRepositoy usuarioRepositoy;
+
+    private final FavoritoRepository favoritoRepository;
 
     public List<CupCakeDTO> buscarTudo() {
         List<CupCake> cupCakes = cupCakeRepository.findAll();
@@ -43,6 +50,23 @@ public class CupCakeService {
 
     public List<CupCakeDTO> buscarTodosProdutos() {
         List<CupCake> cupCakes = cupCakeRepository.buscarTodosProdutos(Tipo.PRODUTO);
+        return cupCakes.stream().map(new CupCakeDTO()::toDTO).toList();
+    }
+
+    public CupCakeDTO buscarPorId(Long id) {
+        Optional<CupCake> cupCake = cupCakeRepository.findById(id);
+        return cupCake.map(cake -> new CupCakeDTO().toDTO(cake)).orElse(null);
+    }
+
+    public List<CupCakeDTO> buscarFavoritosPorUsuario(String email) {
+        Usuario usuario = usuarioRepositoy.findByEmail(email);
+        List<Favorito> favoritos = favoritoRepository.buscarPorUsuario(usuario.getId());
+        List<CupCake> cupCakes = cupCakeRepository.findAll();
+        List<Long> idCupCakes = new ArrayList<>();
+        for (Favorito favorito : favoritos) {
+            idCupCakes.add(favorito.getId().getIdCupCake());
+        }
+        cupCakes = cupCakes.stream().filter(cupCake -> idCupCakes.contains(cupCake.getId())).toList();
         return cupCakes.stream().map(new CupCakeDTO()::toDTO).toList();
     }
 }
