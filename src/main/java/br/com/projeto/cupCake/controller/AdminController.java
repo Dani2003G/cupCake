b6e5b979a225e843b274f7d2e299939119eed269
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-20 19:47:32 -0300

    aDICAO DA OPCAO PARA ARPOVAR AS RECEITAS

--- src/main/java/br/com/projeto/cupCake/controller/AdminController.java
+++ src/main/java/br/com/projeto/cupCake/controller/AdminController.java
@@ -6,6 +6,7 @@ import br.com.projeto.cupCake.service.CupCakeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,7 +23,15 @@ public class AdminController {
     @GetMapping
     public ModelAndView admin() {
         ModelAndView mv = new ModelAndView("admin/home");
+        List<CupCakeDTO> cupCakes = cupCakeService.buscarReceitasNaoAprovadas();
+        mv.addObject("cupCakes", cupCakes);
         return mv;
+    }
+
+    @GetMapping("/aprovar/{id}")
+    public ModelAndView aprovar(@PathVariable Long id) {
+        cupCakeService.aprovar(id);
+        return new ModelAndView("redirect:/admin");
     }
 
 }
