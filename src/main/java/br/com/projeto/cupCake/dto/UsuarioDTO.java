77a8e38a98742a64752cfc1bc4fcce0adf1b55bd
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-20 22:56:40 -0300

    Adicao do atributo dataCadastro e do metodo toDTO

--- src/main/java/br/com/projeto/cupCake/dto/UsuarioDTO.java
+++ src/main/java/br/com/projeto/cupCake/dto/UsuarioDTO.java
@@ -1,11 +1,18 @@
 package br.com.projeto.cupCake.dto;
 
+import br.com.projeto.cupCake.model.Usuario;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
 public class UsuarioDTO {
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    private Long id;
 
     private String nome;
 
@@ -24,5 +31,22 @@ public class UsuarioDTO {
     private String cidade;
 
     private String endereco;
+
+    private String dataCadastro;
+
+    public UsuarioDTO toDTO(Usuario entidade) {
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setId(entidade.getId());
+        usuarioDTO.setNome(entidade.getNome());
+        usuarioDTO.setSobrenome(entidade.getSobrenome());
+        usuarioDTO.setEmail(entidade.getEmail());
+        usuarioDTO.setCpf(entidade.getCpf());
+        usuarioDTO.setDataNascimento(entidade.getDataNascimento().format(formatter));
+        usuarioDTO.setEstado(entidade.getEstado());
+        usuarioDTO.setCidade(entidade.getCidade());
+        usuarioDTO.setEndereco(entidade.getEndereco());
+        usuarioDTO.setDataCadastro(entidade.getDataCadastro().format(formatter));
+        return usuarioDTO;
+    }
 
 }
