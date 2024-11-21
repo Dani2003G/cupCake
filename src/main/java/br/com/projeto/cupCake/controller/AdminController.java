cbf65ba7e92ce7f5914331bfd2c7099d6c671047
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-20 22:55:23 -0300

    Adicao dos metodos para mostrar as paginas de receitas, usuarios, administradores e verUsuario, junto com os metodos de deletar usuario e adm

--- src/main/java/br/com/projeto/cupCake/controller/AdminController.java
+++ src/main/java/br/com/projeto/cupCake/controller/AdminController.java
@@ -1,8 +1,10 @@
 package br.com.projeto.cupCake.controller;
 
 import br.com.projeto.cupCake.dto.CupCakeDTO;
+import br.com.projeto.cupCake.dto.UsuarioDTO;
 import br.com.projeto.cupCake.model.CupCake;
 import br.com.projeto.cupCake.service.CupCakeService;
+import br.com.projeto.cupCake.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -19,6 +22,8 @@ import java.util.List;
 public class AdminController {
 
     private final CupCakeService cupCakeService;
+
+    private final UsuarioService usuarioService;
 
     @GetMapping
     public ModelAndView admin() {
@@ -34,4 +39,47 @@ public class AdminController {
         return new ModelAndView("redirect:/admin");
     }
 
+    @GetMapping("/receitas")
+    public ModelAndView receitas() {
+        ModelAndView mv = new ModelAndView("admin/receitas");
+        List<CupCakeDTO> cupCakes = cupCakeService.buscarReceitasNaoAprovadas();
+        mv.addObject("cupCakes", cupCakes);
+        return mv;
+    }
+
+    @GetMapping("/usuarios")
+    public ModelAndView usuarios() {
+        ModelAndView mv = new ModelAndView("admin/usuarios");
+        List<UsuarioDTO> usuarios = usuarioService.buscarUsuariosComuns();
+        mv.addObject("usuarios", usuarios);
+        return mv;
+    }
+
+    @GetMapping("/deletarUsuario/{id}")
+    public ModelAndView deletarUsuario(@PathVariable Long id) {
+        usuarioService.validarDelecaoUsuario(id);
+        return new ModelAndView("redirect:/admin/usuarios");
+    }
+
+    @GetMapping("/administradores")
+    public ModelAndView administradores() {
+        ModelAndView mv = new ModelAndView("admin/administradores");
+        List<UsuarioDTO> usuarios = usuarioService.buscarUsuariosAdministradores();
+        mv.addObject("usuarios", usuarios);
+        return mv;
+    }
+
+    @GetMapping("/deletarAdm/{id}")
+    public ModelAndView deletarAdm(@PathVariable Long id, Principal principal) {
+        usuarioService.validarDelecaoAdm(id, principal.getName());
+        return new ModelAndView("redirect:/admin/administradores");
+    }
+
+    @GetMapping("/verUsuario/{id}")
+    public ModelAndView verUsuario(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("admin/verUsuario");
+        UsuarioDTO usuarioDTO = usuarioService.buscarPorId(id);
+        mv.addObject("usuario", usuarioDTO);
+        return mv;
+    }
 }
