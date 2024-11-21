6bacc1613f2505ce5815c8722fe94a515f0310e4
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-21 13:51:25 -0300

    Mudado de UsaurioDTO para CadastroUsuarioDTO

--- src/main/java/br/com/projeto/cupCake/controller/CadastroController.java
+++ src/main/java/br/com/projeto/cupCake/controller/CadastroController.java
@@ -1,26 +1,43 @@
 package br.com.projeto.cupCake.controller;
 
+import br.com.projeto.cupCake.dto.CadastroUsuarioDTO;
 import br.com.projeto.cupCake.dto.UsuarioDTO;
+import br.com.projeto.cupCake.repository.UsuarioRepositoy;
 import br.com.projeto.cupCake.service.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/cadastrar")
 public class CadastroController {
 
     private final UsuarioService usuarioService;
 
-    @GetMapping("/cadastrar")
-    public ModelAndView cadastro(UsuarioDTO dto) {
+    @GetMapping
+    public ModelAndView cadastro(CadastroUsuarioDTO dto) {
         return new ModelAndView("cadastro");
     }
 
-    @PostMapping("/cadastrar")
-    public ModelAndView cadastrar(UsuarioDTO dto) {
+    @PostMapping("/novo")
+    public ModelAndView cadastrar(@Valid CadastroUsuarioDTO dto, BindingResult result) {
+        usuarioService.validarCadastro(dto, result);
+
+        if(result.hasErrors()) {
+            return new ModelAndView("cadastro");
+        }
+
         usuarioService.salvar(dto);
         return new ModelAndView("redirect:/login");
     }
