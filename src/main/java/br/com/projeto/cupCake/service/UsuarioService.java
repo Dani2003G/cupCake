d7a7a52fb71be1612e0fd83e9916dbffb7b8d1e6
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-21 13:52:02 -0300

    Na hora de cadastrar fazendo as validacoes dos campos com a adicao do metodo validarCadastro

--- src/main/java/br/com/projeto/cupCake/service/UsuarioService.java
+++ src/main/java/br/com/projeto/cupCake/service/UsuarioService.java
@@ -1,9 +1,11 @@
 package br.com.projeto.cupCake.service;
 
+import br.com.projeto.cupCake.dto.CadastroUsuarioDTO;
 import br.com.projeto.cupCake.dto.UsuarioDTO;
 import br.com.projeto.cupCake.model.Usuario;
 import br.com.projeto.cupCake.repository.UsuarioRepositoy;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,6 +18,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -26,7 +30,7 @@ import java.util.Objects;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class UsuarioService implements UserDetailsService{
+public class UsuarioService implements UserDetailsService {
 
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
@@ -46,7 +50,7 @@ public class UsuarioService implements UserDetailsService{
         return null;
     }
 
-    public void salvar(UsuarioDTO dto) {
+    public void salvar(CadastroUsuarioDTO dto) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         Usuario usuario = new Usuario();
         usuario.setNome(dto.getNome());
@@ -75,7 +79,7 @@ public class UsuarioService implements UserDetailsService{
 
     public void validarDelecaoUsuario(Long id) {
         Usuario usuario = usuarioRepositoy.findById(id).get();
-        if(usuario.getRole().equals("USER")) {
+        if (usuario.getRole().equals("USER")) {
             usuarioRepositoy.delete(usuario);
         }
     }
@@ -87,12 +91,12 @@ public class UsuarioService implements UserDetailsService{
 
     public void validarDelecaoAdm(Long id, String email) {
         List<Usuario> usuarios = usuarioRepositoy.buscarUsuariosAdministradores();
-        if(usuarios.size() == 1) {
+        if (usuarios.size() == 1) {
             throw new ServiceException("Tem que ter ao menos 1 administrador cadastrado");
         }
         Usuario usuario1 = usuarioRepositoy.findById(id).get();
         Usuario usuario2 = usuarioRepositoy.findByEmail(email);
-        if(usuario1.getId().equals(usuario2.getId())) {
+        if (usuario1.getId().equals(usuario2.getId())) {
             throw new ServiceException("Administrador a ser deletado é o logado");
         }
         usuarioRepositoy.delete(usuario1);
@@ -109,14 +113,14 @@ public class UsuarioService implements UserDetailsService{
     }
 
     public void alterarDados(UsuarioDTO dto, String email) {
-        if(!dto.getEmail().equals(email)) {
-            if(Objects.nonNull(usuarioRepositoy.findByEmail(dto.getEmail()))) {
+        if (!dto.getEmail().equals(email)) {
+            if (Objects.nonNull(usuarioRepositoy.findByEmail(dto.getEmail()))) {
                 throw new ServiceException("E-mail já está sendo usado");
             }
         }
         Usuario usuarioLogado = usuarioRepositoy.findByEmail(email);
-        if(!dto.getCpf().equals(usuarioLogado.getCpf())) {
-            if(Objects.nonNull(usuarioRepositoy.findByCpf(dto.getCpf()))) {
+        if (!dto.getCpf().equals(usuarioLogado.getCpf())) {
+            if (Objects.nonNull(usuarioRepositoy.findByCpf(dto.getCpf()))) {
                 throw new ServiceException("CPF já está sendo usado");
             }
         }
@@ -147,13 +151,31 @@ public class UsuarioService implements UserDetailsService{
     public void alterarSenha(UsuarioDTO dto, String email) {
         Usuario usuarioLogado = usuarioRepositoy.findByEmail(email);
         PasswordEncoder encoder = new BCryptPasswordEncoder();
-        if(!encoder.matches(dto.getSenha(), usuarioLogado.getSenha())) {
+        if (!encoder.matches(dto.getSenha(), usuarioLogado.getSenha())) {
             throw new ServiceException("Senha atual incorreta");
         }
-        if(!dto.getNovaSenha().equals(dto.getConfirmarSenha())) {
+        if (!dto.getNovaSenha().equals(dto.getConfirmarSenha())) {
             throw new ServiceException("As novas senhas não conferem");
         }
         usuarioLogado.setSenha(encoder.encode(dto.getNovaSenha()));
         usuarioRepositoy.save(usuarioLogado);
+    }
+
+    public void validarCadastro(@Valid CadastroUsuarioDTO dto, BindingResult result) {
+        if(dto.getSenha().length() < 6 && !result.hasFieldErrors("senha")) {
+            result.addError(new FieldError("dto", "senha", "Senha tem que ter no minimo 6 caracteres"));
+        }
+
+        if (!dto.getSenha().equals(dto.getConfirmarSenha()) && !result.hasFieldErrors("confirmarSenha")) {
+            result.addError(new FieldError("dto", "confirmarSenha", "Senha e confirmar senha não conferem"));
+        }
+
+        if (Objects.nonNull(usuarioRepositoy.findByEmail(dto.getEmail())) && !result.hasFieldErrors("email")) {
+            result.addError(new FieldError("dto", "email", "E-mail já está sendo usado"));
+        }
+
+        if (Objects.nonNull(usuarioRepositoy.findByCpf(dto.getCpf())) && !result.hasFieldErrors("cpf")) {
+            result.addError(new FieldError("dto", "cpf", "CPF já está sendo usado"));
+        }
     }
 }
