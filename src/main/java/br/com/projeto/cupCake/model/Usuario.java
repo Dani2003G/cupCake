dfd6b2a7a69133369464012827a4e9ace089aa0b
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-21 12:23:54 -0300

    Adicao do nullable = false

--- src/main/java/br/com/projeto/cupCake/model/Usuario.java
+++ src/main/java/br/com/projeto/cupCake/model/Usuario.java
@@ -18,27 +18,37 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
+    @Column(nullable = false)
     private String sobrenome;
 
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(unique = true, nullable = false)
     private String cpf;
 
+    @Column(nullable = false)
     private String senha;
 
+    @Column(nullable = false)
     private LocalDate dataNascimento;
 
+    @Column(nullable = false)
     private String estado;
 
+    @Column(nullable = false)
     private String cidade;
 
+    @Column(nullable = false)
     private String endereco;
 
+    @Column(nullable = false)
     private String role;
 
+    @Column(nullable = false)
     private LocalDateTime dataCadastro;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "usuario")
