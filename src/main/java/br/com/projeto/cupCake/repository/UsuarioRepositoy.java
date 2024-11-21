5a50f8fcdaac0439d9f51ea359e073b7f6bc5204
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-20 22:54:34 -0300

    Adicao dos metodos buscarUsuariosComuns e buscarUsuariosAdministradores

--- src/main/java/br/com/projeto/cupCake/repository/UsuarioRepositoy.java
+++ src/main/java/br/com/projeto/cupCake/repository/UsuarioRepositoy.java
@@ -2,11 +2,19 @@ package br.com.projeto.cupCake.repository;
 
 import br.com.projeto.cupCake.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepositoy extends JpaRepository<Usuario, Long> {
 
     Usuario findByEmail(String email);
 
+    @Query("SELECT us FROM Usuario us WHERE us.role = 'USER'")
+    List<Usuario> buscarUsuariosComuns();
+
+    @Query("SELECT us FROM Usuario us WHERE us.role = 'ADMIN'")
+    List<Usuario> buscarUsuariosAdministradores();
 }
