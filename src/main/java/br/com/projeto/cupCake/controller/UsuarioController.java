c9ee73a10dd7fb5f48176f6e9d8ee0854dc35c28
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2024-11-21 16:10:43 -0300

    Adicao de validacoes

--- src/main/java/br/com/projeto/cupCake/controller/UsuarioController.java
+++ src/main/java/br/com/projeto/cupCake/controller/UsuarioController.java
@@ -75,7 +75,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/alterarDados")
-    public ModelAndView alterarDados(Principal principal) {
+    public ModelAndView alterarDados(AlterarDadosDTO dto, Principal principal) {
         ModelAndView mv = new ModelAndView("/usuario/alterarDados");
         UsuarioDTO usuarioDTO = usuarioService.buscarPorEmail(principal.getName());
         mv.addObject("usuarioDTO", usuarioDTO);
@@ -83,7 +83,10 @@ public class UsuarioController {
     }
 
     @PostMapping("/alterarDados")
-    public ModelAndView alterarDados(AlterarDadosDTO dto, Principal principal) {
+    public ModelAndView alterarDados(@Valid AlterarDadosDTO dto, BindingResult result, Principal principal) {
+        if (result.hasErrors()) {
+            return new ModelAndView("/usuario/alterarDados");
+        }
         usuarioService.alterarDados(dto, principal.getName());
         return new ModelAndView("redirect:/usuario");
     }
@@ -96,7 +99,7 @@ public class UsuarioController {
     @PostMapping("/alterarSenha")
     public ModelAndView alterarSenha(@Valid AlterarSenhaDTO dto, BindingResult result, Principal principal) {
         usuarioService.alterarSenha(dto, principal.getName(), result);
-        if(result.hasErrors()) {
+        if (result.hasErrors()) {
             return new ModelAndView("/usuario/alterarSenha");
         }
         return new ModelAndView("redirect:/usuario");
